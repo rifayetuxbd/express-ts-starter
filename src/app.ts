5245f967ef56f87eaa22c629583972d27955f9c4
rifayetuxbd <rifayet@uxd.co.jp>
2024-07-09 10:42:26 +0600

    enable morgan, cors

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get<{}, MessageResponse>('/', (req, res) => {
